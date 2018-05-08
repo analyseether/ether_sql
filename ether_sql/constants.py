cb54849960d0981d866614bac47a4db39760a59b
Your Nameclear <ankitchiplunkar@gmail.com>
2018-05-08 16:40:29 +0200

    added few ether specific constants

---
+++ ether_sql/constants.py
@@ -0,0 +1,9 @@
+PreByzantiniumReward = 5*10**18
+PostByzantiniumReward = 3*10**18
+# source https://github.com/ethereum/go-ethereum/blob/44d40ffce1200ce8875e187b57ac17e4901db32a/params/config.go
+FORK_BLOCK_NUMBER = {'Homestead': 1150000,
+                     'DAOFork': 1920000,
+                     'EIP150': 2463000,
+                     'EIP155': 2675000,
+                     'EIP158': 2675000,
+                     'Byzantium': 4370000}
