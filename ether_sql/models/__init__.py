e70df0e2120c4b7fdf56e333d810cd62f34e6564
Your Nameclear <ankitchiplunkar@gmail.com>
2018-05-08 13:55:57 +0200

    reoved sqlalchemy.exc.InvalidRequestError error

--- ether_sql/models/__init__.py
+++ ether_sql/models/__init__.py
@@ -2,3 +2,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 base = declarative_base()
+
+# Initializing these classes here to remove sqlalchemy.exc.InvalidRequestError
+# More here: https://stackoverflow.com/a/45613994/3420738
+from ether_sql.models.blocks import Blocks
+from ether_sql.models.transactions import Transactions
+from ether_sql.models.uncles import Uncles
+from ether_sql.models.receipts import Receipts
+from ether_sql.models.logs import Logs
+from ether_sql.models.traces import Traces
