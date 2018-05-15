c4f844101306a02015fa7c4e34bdf2262aceffcd
Your Nameclear <ankitchiplunkar@gmail.com>
2018-05-15 15:24:30 +0200

    added new test cases for pushing a range of blocks

--- ether_sql/tests/conftest.py
+++ ether_sql/tests/conftest.py
@@ -2,7 +2,6 @@ from ether_sql import setup_db_engine
 from ether_sql import base
 from ether_sql import settings
 import pytest
-from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY settings
 TEST_SQLALCHEMY_DB = 'ether_sql_tests'
@@ -14,22 +13,20 @@ def empty_sql_engine():
     Fixture containing exmpty database
     """
     empty_sql_engine = setup_db_engine(user=settings.SQLALCHEMY_USER,
-                                        password=settings.SQLALCHEMY_PASSWORD,
-                                        db=TEST_SQLALCHEMY_DB)
+                                       password=settings.SQLALCHEMY_PASSWORD,
+                                       db=TEST_SQLALCHEMY_DB)
     return empty_sql_engine
 
 
 @pytest.fixture()
-def empty_table_session():
+def empty_table_engine():
     """
     Fixture with created but empty tables
     """
-    db_engine = setup_db_engine(user=settings.SQLALCHEMY_USER,
-                                password=settings.SQLALCHEMY_PASSWORD,
-                                db=TEST_SQLALCHEMY_DB)
+    empty_table_engine = setup_db_engine(user=settings.SQLALCHEMY_USER,
+                                         password=settings.SQLALCHEMY_PASSWORD,
+                                         db=TEST_SQLALCHEMY_DB)
 
-    base.metadata.create_all(db_engine)
+    base.metadata.create_all(empty_table_engine)
 
-    DBSession = sessionmaker(bind=db_engine)
-    empty_table_session = DBSession()
-    return empty_table_session
+    return empty_table_engine
--- ether_sql/tests/test_sql.py
+++ ether_sql/tests/test_sql.py
@@ -1,5 +1,8 @@
 from ether_sql.models import base
-from ether_sql.scrapper import add_block_number
+from ether_sql.models import Blocks, Transactions, Receipts, Uncles
+from ether_sql.scrapper import add_block_number, scrape_blocks
+from sqlalchemy import func
+from sqlalchemy.orm import sessionmaker
 
 
 class TestEmptyDB():
@@ -15,6 +18,45 @@ class TestEmptyDB():
 
 class TestEmptyTables():
 
-    def test_push_block(self, empty_table_session):
-        session = add_block_number(block_number=0, session=empty_table_session)
-        session.commit()
+    def test_push_block_0(self, empty_table_engine):
+
+        DBSession = sessionmaker(bind=empty_table_engine)
+        empty_table_session = DBSession()
+
+        test_db_session = add_block_number(block_number=0, session=empty_table_session)
+        test_db_session.commit()
+
+        # assert the maximum block number
+        assert test_db_session.query(
+                func.max(Blocks.block_number)).scalar() == 0
+        # number of rows in Blocks
+        assert test_db_session.query(Blocks).count() == 1
+        # number of rows in uncles
+        assert test_db_session.query(Uncles).count() == 0
+        # number of rows in transactions
+        assert test_db_session.query(Transactions).count() == 0
+        # number of rows in receipts
+        assert test_db_session.query(Receipts).count() == 0
+        test_db_session.close()
+
+        base.metadata.drop_all(empty_table_engine)
+
+    def test_push_block_range(self, empty_table_engine):
+        DBSession = sessionmaker(bind=empty_table_engine)
+        empty_table_session = DBSession()
+
+        scrape_blocks(sql_block_number=46140, node_block_number=46150,
+                      session=empty_table_session)
+        # assert the maximum block number
+        assert empty_table_session.query(
+                func.max(Blocks.block_number)).scalar() == 46150
+        # number of rows in Blocks
+        assert empty_table_session.query(Blocks).count() == 10
+        # number of rows in uncles
+        assert empty_table_session.query(Uncles).count() == 1
+        # number of rows in transactions
+        assert empty_table_session.query(Transactions).count() == 1
+        # number of rows in receipts
+        assert empty_table_session.query(Receipts).count() == 1
+        empty_table_session.close()
+        base.metadata.drop_all(empty_table_engine)
