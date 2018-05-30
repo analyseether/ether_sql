b456207f55ef04f89cfa8efb5500a7a083c42a68
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2018-05-30 15:20:41 +0200

    first commit for managing migrations of database

---
+++ ether_sql/migrations/env.py
@@ -0,0 +1,78 @@
+from __future__ import with_statement
+from alembic import context
+from sqlalchemy import engine_from_config, pool, create_engine
+# from logging.config import fileConfig
+from ether_sql.models import base
+import logging
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+# fileConfig(config.config_file_name)
+logger = logging.getLogger(__name__)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.def get_url():
+
+
+def get_url():
+    "Database URL must be specified on command line with -x url=<DB_URL>"
+    url = context.get_x_argument(as_dictionary=True).get('url')
+    return url
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    connectable = create_engine(get_url(), client_encoding='utf8')
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
