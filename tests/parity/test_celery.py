42981d3e990a4c0a2fe84f91dc8f91b864d8bdf6
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2018-06-13 10:57:39 +0200

    added tests for redis celery queue

---
+++ tests/parity/test_celery.py
@@ -0,0 +1,9 @@
+from tests.common_tests.celery import (
+    push_block_range_multiple_threads,
+)
+
+
+def test_parity_push_block_range_multiple_threads(parity_start_celery,
+                                                  parity_settings):
+    push_block_range_multiple_threads(parity_settings)
+    pass
--- tests/parity/test_cli.py
+++ tests/parity/test_cli.py
@@ -1,19 +1,19 @@
 from tests.common_tests.cli import (
-    export_to_csv,
+    export_to_csv_single_thread,
     verify_block_contents,
-    push_block_range,
+    push_block_range_single_thread,
 )
 
 
 def test_parity_push_block_range(parity_settings):
-    push_block_range(parity_settings)
+    push_block_range_single_thread(parity_settings)
     pass
 
 
 class TestParityBlock56160():
 
     def test_parity_export_to_csv(self, parity_session_block_56160):
-        export_to_csv(parity_session_block_56160)
+        export_to_csv_single_thread(parity_session_block_56160)
         pass
 
     def test_parity_verify_block_contents(self, parity_session_block_56160):
