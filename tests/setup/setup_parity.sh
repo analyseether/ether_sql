c120d0812e7d972c0ca4e85f51081338ba3f1645
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2018-08-28 14:27:33 +0200

    added testing releases as trusted

--- tests/setup/setup_parity.sh
+++ tests/setup/setup_parity.sh
@@ -1,6 +1,9 @@
-bash <(curl https://get.parity.io -L) -r stable
-echo "deb http://ftp.debian.org/debian testing main" > testing.list && sudo mv testing.list /etc/apt/sources.list.d/
+# bash <(curl https://get.parity.io -L) -r stable
+curl -O "https://d1h4xl4cr1h0mo.cloudfront.net/v1.11.7/x86_64-unknown-debian-gnu/parity"
+chmod +x parity
+echo "deb [trusted=yes] http://ftp.debian.org/debian testing main" > testing.list && sudo mv testing.list /etc/apt/sources.list.d/
 sudo apt update
 sudo apt-get -t testing install libstdc++6
 curl -O https://storage.googleapis.com/ether_sql/export_blocks_mainnet.rlp
-parity import ./export_blocks_mainnet.rlp --tracing on
+./parity import ./export_blocks_mainnet.rlp --tracing on
+./parity &
