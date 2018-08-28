a7a1db12e11471c449abb3615c03f7e62289c46d
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2018-08-28 14:34:15 +0200

    fix old version of dpkg

--- tests/setup/setup_parity.sh
+++ tests/setup/setup_parity.sh
@@ -1,4 +1,6 @@
 # bash <(curl https://get.parity.io -L) -r stable
+mkdir .parity-bin
+cd .parity-bin
 curl -O "https://d1h4xl4cr1h0mo.cloudfront.net/v1.11.7/x86_64-unknown-debian-gnu/parity"
 chmod +x parity
 echo "deb [trusted=yes] http://ftp.debian.org/debian testing main" > testing.list && sudo mv testing.list /etc/apt/sources.list.d/
